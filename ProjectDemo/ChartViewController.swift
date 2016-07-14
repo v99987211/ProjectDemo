85487b08afd430e16a20a8163c6db5f2531a5191
萬家辰 <wanjiachen@wanjiachendemacbook-pro.local>
2016-07-14 20:05:06 +0800

    Chart test

---
+++ ProjectDemo/ChartViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ChartViewController.swift
+//  ProjectDemo
+//
+//  Created by 萬家辰 on 2016/7/14.
+//  Copyright © 2016年 Checkmate. All rights reserved.
+//
+
+import Foundation
+import Charts
+
+class ChartViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    
+}
